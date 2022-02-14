9b91e7d8d96d4668b771bf78ab88eaeef84a2170
Abdulrahman Aboudaoud <abdulrahmanaboudaoud@gmail.com>
2022-02-14 14:14:44 +0100

    Database gekoppeld en  totaal aantal characters zichtbaar zijn op de plaats van [X]

--- vormgeving/index.php
+++ vormgeving/index.php
@@ -1,3 +1,25 @@
+<?php
+$db = 'mysql:host=localhost;dbname=dynamische_content';
+$user = 'root';
+$pass = 'mysql';
+try {
+    $conn = new PDO($db, $user, $pass);
+    echo 'u are connected';
+    
+} catch (PDOException $e) {
+    echo "failed" . $e->getMessage();
+    
+}
+
+$sql = "SELECT COUNT(id) AS total FROM `characters`;";
+$result = $conn->query($sql);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +29,10 @@
     <link href="resources/css/style.css" rel="stylesheet"/>
 </head>
 <body>
-<header><h1>Alle [X] characters uit de database</h1>
+<header>
+
+
+    <h1>Alle [<?php echo $row['total']; ?>] characters uit de database</h1>
 
 </header>
 <div id="container">
@@ -28,6 +53,13 @@
         <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
     </a>
 </div>
-<footer>&copy; [jenaam] 2020</footer>
+<footer>&copy; [Abdulrahman AbouDaoud] 2022</footer>
 </body>
 </html>
+
+
+<<?php 
+
+}
+
+ ?>
